95ee8a642c209181bcd644c1fdc59fbd0d1b8227
sunginHwang <gommpo111@gmail.com>
2018-12-30 16:25:32 +0900

    rn props component 공부 중 이거 은근 꿀잼.

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+const LineText = ({
+                      name
+                  }) => {
+    return (
+        <View style={{alignItems: 'center'}}>
+            <Text>Hello {name}!</Text>
+        </View>
+    )
+};
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up 성인 App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return (
+            <View style={styles.container}>
+                <LineText name='Open up 성인 App.js to start working on your app!' />
+                <LineText name='Open up HeaJin App.js to start working on your app!' />
+                <Image source={pic} style={{width: 500, height: 110}}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderColor: '#eee',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        borderColor: '#eee',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
