f7f392f0b3b9db5aa371bb91d6f8bdb694e2bffc
sunginHwang <gommpo111@gmail.com>
2019-01-01 17:35:02 +0900

    플랫폼 별 처리 작업

--- HalfView.js
+++ HalfView.js
@@ -1,15 +1,18 @@
-import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, Platform} from 'react-native';
 import React, {Component} from 'react';
 
 const {width} = Dimensions.get('window');
 export default class HalfView extends Component {
 
 
+
     render() {
+        const targetOs = Platform.OS === 'ios' ? 'ios' : 'android';
+
         return (
             <View style={styles.halfViewWrapper}>
-                <View style={styles.halfView}><Text>1{this.props.msg}</Text></View>
-                <View style={styles.halfView}><Text>2{this.props.msg}</Text></View>
+                <View style={styles.halfView}><Text>{targetOs + ' '+ this.props.msg}</Text></View>
+                <View style={styles.halfView}><Text>{targetOs + ' '+ this.props.msg}</Text></View>
             </View>
         )
     }
@@ -24,7 +27,14 @@ const styles = StyleSheet.create({
     halfView: {
         width: width / 2,
         height: 30,
-        backgroundColor: 'skyblue',
+        ...Platform.select({
+            ios: {
+                backgroundColor: 'skyblue',
+            },
+            android: {
+                backgroundColor: 'blue',
+            },
+        }),
         color:'#fff',
         alignItems: 'center'
     },
