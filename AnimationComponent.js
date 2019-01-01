db33a1021552f4b0989c02abef77238e73c07fcd
sunginHwang <gommpo111@gmail.com>
2019-01-01 18:05:43 +0900

    animationComponentWrapper

---
+++ AnimationComponent.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { Animated } from 'react-native';
+
+class AnimationComponent extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+        };
+    }
+
+    componentDidMount() {
+        Animated.timing(                  // Animate over time
+            this.state.fadeAnim,            // The animated value to drive
+            {
+                toValue: 1,                   // Animate to opacity: 1 (opaque)
+                duration: 2000,              // Make it take a while
+            }
+        ).start();                        // Starts the animation
+    }
+
+    render() {
+        let { fadeAnim } = this.state;
+
+        return (
+            <Animated.View                 // Special animatable View
+                style={{
+                    ...this.props.style,
+                    opacity: fadeAnim,         // Bind opacity to animated value
+                }}
+            >
+                {this.props.children}
+            </Animated.View>
+        );
+    }
+}
+export default AnimationComponent;
--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Image, ScrollView} from 'react-native';
+import {StyleSheet, View, Image, ScrollView, Dimensions} from 'react-native';
 import LineText from './LineText';
 import HalfView from './HalfView';
 import InputHandle from './InputHandle';
 import ClientNetworkHandle from './ClientNetworkHandle';
+import AnimationComponent from './AnimationComponent';
+const {width} = Dimensions.get('window');
 
 export default class App extends Component {
     render() {
@@ -13,8 +15,11 @@ export default class App extends Component {
         return (
             <View style={styles.container}>
                 <Image source={pic} style={{width: 500, height: 400}}/>
-                <ClientNetworkHandle/>
                 <InputHandle/>
+                <ClientNetworkHandle/>
+                <AnimationComponent style={{width:width , height:3}}>
+                    <HalfView msg='FadeInHalfViewTest'/>
+                </AnimationComponent>
                 <ScrollView
                     contentContainerStyle={styles.scrollContainer}>
                     <LineText name='Open up 성인 App.js to start working on your app!'/>
@@ -22,6 +27,7 @@ export default class App extends Component {
                     <LineText name='Open up 상민 sApp.js to start working on your app!'/>
                     <HalfView msg='HalfViewTest'/>
                 </ScrollView>
+
             </View>
         );
     }
